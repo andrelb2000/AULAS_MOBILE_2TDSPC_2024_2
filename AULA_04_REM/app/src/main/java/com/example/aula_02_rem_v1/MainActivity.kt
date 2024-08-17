a27232200f7af6fc750311afaa57f713d3692fc7
Prof. André Luiz Braga, D.Sc <andre.luiz.braga@terra.com.br>
2024-08-17 13:38:05 -0300

    dfgdgfdf

---
+++ AULA_04_REM/app/src/main/java/com/example/aula_02_rem_v1/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.aula_02_rem_v1
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private  var xModelo: Int = 0
+    private  var yModelo: Int = 0
+    private  var viewX: TextView? = null
+    private  var eq1:   TextView? = null
+    private  var coefAEq: EditText? = null
+
+    private fun inicializar(){
+        val botaoUp: Button = findViewById(R.id.ID1_XUp_button)
+        viewX = findViewById(R.id.ID1_XMod_textView)
+        eq1   = findViewById(R.id.ID1_EQ1_textView)
+        coefAEq = findViewById(R.id.ID1_Eq1CoefA_editTextText)
+
+        botaoUp.setOnClickListener {
+            xModelo = xModelo + 1
+            viewX?.text = "Valor X ${xModelo}"
+
+            var eq1Temp: Int = 0
+            var coefATemp: Int = 0
+            coefATemp = coefAEq?.text.toString().toInt()
+            eq1Temp = coefATemp   * xModelo + 3*yModelo
+            eq1?.text = eq1Temp.toString()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        inicializar()
+    }
+}
